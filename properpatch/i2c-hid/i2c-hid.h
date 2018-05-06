55192726d3b0b4c7cd6bded55a8ab50bf51c5554
Julian Sax <jsbc@gmx.de>
2018-05-07 00:34:10 +0200

    changed to CONFIG_DMI and renamed i2c-hid-core

--- properpatch/i2c-hid/i2c-hid.h
+++ properpatch/i2c-hid/i2c-hid.h
@@ -2,7 +2,7 @@
 #define I2C_HID_H
 
 
-#ifdef CONFIG_X86
+#ifdef CONFIG_DMI
 struct i2c_hid_desc *i2c_hid_get_dmi_i2c_hid_desc_override(void);
 char *i2c_hid_get_dmi_hid_report_desc_override(unsigned int *size);
 #else
